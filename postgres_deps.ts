48e3b9a9492463bc03d93f2df20d2223e1a57d75
Kyle June <github@kylejune.com>
2022-11-12 19:15:39 -0600

    Upgrade deno and dependencies (#7)

--- postgres_deps.ts
+++ postgres_deps.ts
@@ -2,5 +2,5 @@ export {
   Client,
   Transaction,
   TransactionError,
-} from "https://deno.land/x/postgres@v0.16.1/mod.ts";
-export type { ClientOptions } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
+} from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+export type { ClientOptions } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
--- test_deps.ts
+++ test_deps.ts
@@ -5,17 +5,17 @@ export {
   assertRejects,
   assertStrictEquals,
   assertThrows,
-} from "https://deno.land/std@0.150.0/testing/asserts.ts";
+} from "https://deno.land/std@0.163.0/testing/asserts.ts";
 
-export { ensureDir } from "https://deno.land/std@0.150.0/fs/ensure_dir.ts";
+export { ensureDir } from "https://deno.land/std@0.163.0/fs/ensure_dir.ts";
 
-export { describe, it } from "https://deno.land/std@0.150.0/testing/bdd.ts";
+export { describe, it } from "https://deno.land/std@0.163.0/testing/bdd.ts";
 
 export {
   assertSpyCall,
   assertSpyCalls,
   spy,
   stub,
-} from "https://deno.land/std@0.150.0/testing/mock.ts";
+} from "https://deno.land/std@0.163.0/testing/mock.ts";
 
-export { FakeTime } from "https://deno.land/std@0.150.0/testing/time.ts";
+export { FakeTime } from "https://deno.land/std@0.163.0/testing/time.ts";
